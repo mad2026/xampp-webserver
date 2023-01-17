55e02fd205be8822d88fd40bd69ee1f71995aacb
mad2026 <77991214+mad2026@users.noreply.github.com>
2023-01-17 09:51:30 -0600

    me in my infinite wisdom forgot <br> tags

--- product_discount_app2/display_discount.php
+++ product_discount_app2/display_discount.php
@@ -16,33 +16,36 @@
     
     // escape the unformatted input
     $product_description_escaped = htmlspecialchars($product_description);
+    
+    echo '
+    <!DOCTYPE html>
+        <html>
+            <head>
+                <title>Product Discount Calculator</title>
+                <link rel="stylesheet" type="text/css" href="main.css">
+            </head>
+    
+            <body>
+                <main>
+                    <h1>Product Discount Calculator</h1>
+                
+                    <label>Product Description:</label>
+                    <span>'.$product_description_escaped.'</span><br>
+                
+                    <label>List Price:</label>
+                    <span>'.$list_price_formatted.'</span><br>
+                
+                    <label>Standard Discount:</label>
+                    <span>'.$discount_percent_formatted.'</span><br>
+                    
+                    <label>Discount Amount:</label>
+                    <span>'.$discount_formatted.'</span><br>
+                
+                    <label>Discount Price:</label>
+                    <span>'.$discount_price_formatted.'</span><br>
+
+                </main>
+            </body>
+        </html>
+    ';
 ?>
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Product Discount Calculator</title>
-    <link rel="stylesheet" type="text/css" href="main.css">
-</head>
-
-<body>
-    <main>
-        <h1>Product Discount Calculator</h1>
-
-        <label>Product Description:</label>
-        <span><?php echo $product_description_escaped; ?></span><br>
-
-        <label>List Price:</label>
-        <span><?php echo $list_price_formatted; ?></span>
-
-        <label>Standard Discount:</label>
-        <span><?php echo $discount_percent_formatted; ?></span>
-        
-        <label>Discount Amount:</label>
-        <span><?php echo $discount_formatted; ?></span>
-        
-        <label>Discount Price:</label>
-        <span><?php echo $discount_price_formatted; ?></span>
-
-    </main>
-</body>
-</html>
