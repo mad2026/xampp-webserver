c0a399c4809a2a46fc353e2d6bd7ff3e67a9b00b
mad2026 <77991214+mad2026@users.noreply.github.com>
2023-01-17 10:47:19 -0600

    made JS file for validating user input Client Side

--- product_discount_app2/index.php
+++ product_discount_app2/index.php
@@ -2,23 +2,24 @@
 <html>
 <head>
     <title>Product Discount Calculator</title>
+    <script src="productDiscount.js"></script>
     <link rel="stylesheet" type="text/css" href="main.css">
 </head>
 
 <body>
     <main>
         <h1>Product Discount Calculator</h1>
-        <form action="display_discount.php" method="post">
+        <form action="display_discount.php" onsubmit="return validateProductData();" method="post">
 
             <div id="data">
                 <label>Product Description:</label>
-                <input type="text" name="product_description"><br>
+                <input type="text" name="product_description" id="product_description" required><br>
 
                 <label>List Price:</label>
-                <input type="text" name="list price"><br>
+                <input type="number" name="list_price" id="list_price"  min="0" required><br>
 
-                <label>Discount Percent:</label>
-                <input type="text" name="discount_percent">
+                <label>Discount Percent (0-100):</label>
+                <input type="range" name="discount_percent" id="discount_percent" min="0" max="100" required>
                 <span>%</span><br>
             </div>
             <div id="buttons">
