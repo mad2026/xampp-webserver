4929cf67852986a7941f7c64d2480d571bf5a914
mad2026 <77991214+mad2026@users.noreply.github.com>
2023-02-16 10:28:46 -0600

    moved everthing into the CIS4033 folder

---
+++ CIS4033/MasonDavenportAssignment2/app_config.php
@@ -0,0 +1,8 @@
+<?php 
+//application folder name on your web root or htdocs folder
+define('APP_ROOT',$_SERVER['DOCUMENT_ROOT']);
+//DEBUG echo (APP_ROOT);
+define('APP_FOLDER_NAME', '/htdocs/MasonDavenportAssignment2');
+define('WEB_ROOT','http://'.$_SERVER['SERVER_NAME']);
+//DEBUG echo(WEB_ROOT);
+?>
